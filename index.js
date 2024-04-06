c1002bc8ef25dfedf9baa4d405a344d764b3d40f
idtien <phttien203@gmail.com>
2024-04-06 20:18:39 +0700

    introduce myself

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const port = 3000
 
 app.get('/', (req,res)=> res.send('<h1>Hello World!</h1>'))
 app.get('/about', (req,res)=> res.send('<h1>Hello Im A FE Developer!</h1>'))
+app.get('/info', (req,res)=> res.send('<h1>My name is Tien!</h1>'))
 app.listen(port, ()=> console.log(`Example app listening at port ${port}`))
