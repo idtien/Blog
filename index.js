78495792ccf88e72cde9d46b95184f90f444071f
idtien <phttien203@gmail.com>
2024-04-06 20:16:34 +0700

    hello im a fe dev

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req,res)=> res.send('<h1>Hello World!</h1>'))
+app.get('/about', (req,res)=> res.send('<h1>Hello Im A FE Developer!</h1>'))
 app.listen(port, ()=> console.log(`Example app listening at port ${port}`))
