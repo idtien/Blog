dd889ac75916bb00158dd29786191eba00eacaa2
idtien <phttien203@gmail.com>
2024-04-07 22:17:08 +0700

    form default behavior

--- src/index.js
+++ src/index.js
@@ -20,9 +20,13 @@ app.set('views', path.join(__dirname, 'resources/views'))
 app.get('/', (req,res)=> res.render('home'))
 app.get('/about', (req,res)=> res.send('<h1>Hello Im A FE Developer!</h1>'))
 app.get('/address', (req,res)=> res.send('<h1>Hue - VietNam!</h1>'))
-app.get('/address', (req,res)=> res.send('<h1>Gender: Male</h1>'))
+app.post('/address', (req,res)=> res.send('<h1>Gender: Male</h1>'))
 
 app.get('/search', (req,res)=> {
+    console.log('res', req.query)
+    res.render('search')
+})
+app.post('/search', (req,res)=> {
     console.log('res', req.query)
     res.render('search')
 })
